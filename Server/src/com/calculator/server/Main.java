5fe9c6f4c476e69e2120229738a522a5cb8d2d9c
dumrelu <dum.relu@gmail.com>
2015-12-27 19:47:40 +0200

    RMI test server.

---
+++ Server/src/com/calculator/server/Main.java
@@ -0,0 +1,21 @@
+package com.calculator.server;
+
+import com.calculator.common.Constants;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Main 
+{
+
+    public static void main(String[] args) throws RemoteException 
+    {
+        Registry registry = LocateRegistry.createRegistry(Constants.PORT);
+        
+        Calculator calculator = new Calculator();
+        registry.rebind(Constants.CALCULATOR_NAME, calculator);
+        
+        System.out.println("Server started!");
+    }
+    
+}
